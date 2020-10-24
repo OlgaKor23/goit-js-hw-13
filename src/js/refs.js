c28214743570017c1d8ca94f57a5c115e4a6a92c
OlgaKor23 <work.olga.koryshkina@gmail.com>
2020-10-24 14:01:52 +0300

    reuploaded module 13

---
+++ src/js/refs.js
@@ -0,0 +1,1 @@
+const refs = {    photoCardsContainer: document.querySelector('.gallery'),    searchForm: document.querySelector('.search-form'),    spinner: document.querySelector('.js-spinner'),    loadMoreBtn: document.querySelector('[data-action="load-more"]'),};export default refs;
